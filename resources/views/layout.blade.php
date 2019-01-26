cfcf9f0831d9f2751b89885e492368c150aaa9ce
workholic <sl.vilcapuma.edwin@gmail.com>
2019-01-26 09:21:34 -0500

    clase 5

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1 class="page-header text-center">Crud Laravel 5.5</h1>
+        @yield('content')
+      </div>
+
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  
+
+    </body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+    <div class="col-sm-8">
+        listado del producto
+    </div>
+    <div class="col-sm-4">
+        mensaje
+    </div>
+@endsection
